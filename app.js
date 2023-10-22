37d6c9ebd0dc33564a3dfccc1889af356be2f108
Kirill-Fokin <kirill19011998@mail.ru>
2023-10-22 13:28:44 +0300

    feat: add class Note

---
+++ app.js
@@ -0,0 +1,74 @@
+console.log(3242345)
+
+class Note {
+  _name = "";
+  _done = false;
+
+  constructor(container, name="", done=false) {
+    this.item = document.createElement("div");
+    this.buttonGroup = document.createElement("div")
+    this.nameSpan = document.createElement("span")
+    this.doneButton = document.createElement("button")
+    this.deleteButton = document.createElement("button")
+
+   this.item.classList.add('note')
+   container.append(this.item)
+   
+   this.buttonGroup.classList.add("group")
+   this.doneButton.classList.add("btn","done-btn")
+   this.doneButton.textContent = "Готово"
+   this.deleteButton.classList.add("btn", "delete-btn")
+   this.deleteButton.textContent = "Удалить"
+
+   this.deleteButton.addEventListener('click', ()=> {
+     if (confirm('вы уверены?')) {
+        this.item.remove()
+     }
+   })
+
+
+   this.doneButton.addEventListener('click', ()=> {
+    this.done = !this.done
+  })
+
+   this.name = name;
+   this.done = done;
+   
+   this.buttonGroup.append(this.doneButton)
+   this.buttonGroup.append(this.deleteButton)
+   this.item.append(this.nameSpan)
+   this.item.append(this.buttonGroup)
+   container.append(this.item)
+  }
+
+  set name (value) {
+    this._name = value;
+    this.nameSpan.textContent = value;
+    console.log('сделано')
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set done(value) {
+    this._done = value;
+
+    if (value) {
+      this.item.classList.add('done-btn')
+    } else {
+        this.item.classList.remove('done-btn')
+    }
+
+  }
+
+  get done () {
+    return this._done;
+  }
+
+}
+
+let note = new Note(document.getElementById('app'),'Hello')
+
+console.log(note)
+
